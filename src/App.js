4e9b811de3ac4b697c94662127f9896d5d4ade60
BahnMiFPS <87252277+BahnMiFPS@users.noreply.github.com>
2023-04-18 21:43:58 +1000

    vu

--- src/App.js
+++ src/App.js
@@ -1,30 +1,33 @@
-import React from "react";
-import SearchAppBar from "./components/SearchAppBar";
-import HomePage from "./pages/HomePage";
-import { Route, Routes } from "react-router-dom";
-import { ThemeProvider, createTheme } from "@mui/material/styles";
-import CssBaseline from "@mui/material/CssBaseline";
-import DetailPage from "./pages/DetailPage";
-import SignInPage from "./pages/SignInPage";
+import React, { useContext } from "react"
+import SearchAppBar from "./components/SearchAppBar"
+import HomePage from "./pages/HomePage"
+import { Route, Routes } from "react-router-dom"
+import { ThemeProvider, createTheme } from "@mui/material/styles"
+import CssBaseline from "@mui/material/CssBaseline"
+import DetailPage from "./pages/DetailPage"
+import SignInPage from "./pages/SignInPage"
+import AuthContext from "./auth/AuthContext"
 
 const darkTheme = createTheme({
-  palette: {
-    mode: "dark",
-  },
-});
+	palette: {
+		mode: "dark",
+	},
+})
 
 function App() {
-  return (
-    <ThemeProvider theme={darkTheme}>
-      <CssBaseline />
-      <SearchAppBar />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="signInPage" element={<SignInPage />} />
-        <Route path="pageDetail/:id" element={<DetailPage />} />
-      </Routes>
-    </ThemeProvider>
-  );
+	const auth = useContext(AuthContext)
+	console.log("from app.js", auth)
+	return (
+		<ThemeProvider theme={darkTheme}>
+			<CssBaseline />
+			<SearchAppBar />
+			<Routes>
+				<Route path="/" element={<HomePage />} />
+				<Route path="signInPage" element={<SignInPage />} />
+				<Route path="pageDetail/:id" element={<DetailPage />} />
+			</Routes>
+		</ThemeProvider>
+	)
 }
 
-export default App;
+export default App
--- src/pages/SignInPage.js
+++ src/pages/SignInPage.js
@@ -1,129 +1,136 @@
-import { LoadingButton } from "@mui/lab";
+import { LoadingButton } from "@mui/lab"
 import {
-  Container,
-  IconButton,
-  InputAdornment,
-  Link,
-  Modal,
-  Stack,
-  Typography,
-} from "@mui/material";
-import React, { useState } from "react";
-import LockIcon from "@mui/icons-material/Lock";
-import { FormProvider } from "../components/form";
-import { FTextField } from "../components/form";
-import { useForm } from "react-hook-form";
-import { Visibility, VisibilityOff } from "@mui/icons-material";
+	Container,
+	IconButton,
+	InputAdornment,
+	Link,
+	Modal,
+	Stack,
+	Typography,
+} from "@mui/material"
+import React, { useContext, useState } from "react"
+import LockIcon from "@mui/icons-material/Lock"
+import { FormProvider } from "../components/form"
+import { FTextField } from "../components/form"
+import { useForm } from "react-hook-form"
+import { Visibility, VisibilityOff } from "@mui/icons-material"
+import AuthContext from "../auth/AuthContext"
+import { useNavigate } from "react-router-dom"
 const SignInPage = () => {
-  const defaultValues = {
-    username: "web virgil learner",
-    email: "bao@gmail.com",
-    password: "123456",
-    remember: true,
-    choise: [],
-    gender: [],
-  };
-  const methods = useForm({ defaultValues });
-  const {
-    //reset,
-    //setError,
-    handleSubmit,
-    //control,
-    formState: { errors, isSubmitting },
-  } = methods;
-  const [showPassword, setShowPassword] = useState(false);
+	const auth = useContext(AuthContext)
 
-  const onSubmit = (data) => {
-    console.log(data);
-    //setError("afterSubmit", {message:"Server response error"})
-  };
-  const [open, setOpen] = React.useState(true);
-  const handleOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false);
-  return (
-    <Container
-      sx={{
-        minHeight: 500,
-        maxWidth: 450,
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Modal
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
-        sx={{ display: "flex", justifyContent: "center", alignItems: "center" }}
-      >
-        <Container
-          sx={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            flexDirection: "column",
-            border: 1,
-            padding: 2,
-            width: 450,
-            backgroundColor: "green",
-          }}
-        >
-          <LockIcon sx={{ mb: 2 }} />
-          <Typography sx={{ mb: 2 }}>Log In</Typography>
+	const defaultValues = {
+		username: "web virgil learner",
+		email: "bao@gmail.com",
+		password: "123456",
+		remember: true,
+		choise: [],
+		gender: [],
+	}
+	const methods = useForm({ defaultValues })
+	const {
+		//reset,
+		//setError,
+		handleSubmit,
+		//control,
+		formState: { errors, isSubmitting },
+	} = methods
+	const [showPassword, setShowPassword] = useState(false)
+	const navigate = useNavigate()
+	const onSubmit = (data) => {
+		auth.singin(data.username, () => {
+			navigate("/")
+		})
+		console.log(data)
+		//setError("afterSubmit", {message:"Server response error"})
+	}
+	const [open, setOpen] = React.useState(true)
+	const handleOpen = () => setOpen(true)
+	const handleClose = () => setOpen(false)
+	return (
+		<Container
+			sx={{
+				minHeight: 500,
+				maxWidth: 450,
+				display: "flex",
+				justifyContent: "center",
+				alignItems: "center",
+			}}
+		>
+			<Modal
+				open={open}
+				onClose={handleClose}
+				aria-labelledby="modal-modal-title"
+				aria-describedby="modal-modal-description"
+				sx={{ display: "flex", justifyContent: "center", alignItems: "center" }}
+			>
+				<Container
+					sx={{
+						display: "flex",
+						justifyContent: "center",
+						alignItems: "center",
+						flexDirection: "column",
+						border: 1,
+						padding: 2,
+						width: 450,
+						backgroundColor: "green",
+					}}
+				>
+					<LockIcon sx={{ mb: 2 }} />
+					<Typography sx={{ mb: 2 }}>Log In</Typography>
 
-          <FormProvider methods={methods} onSubmit={handleSubmit(onSubmit)}>
-            <Stack spacing={3}>
-              <FTextField name="username" label="UserName" />
-              <FTextField
-                name="password"
-                label="password"
-                type={showPassword ? "text" : "password"}
-                InputProps={{
-                  endAdornment: (
-                    <InputAdornment position="end">
-                      <IconButton
-                        aria-label="toggle password visibility"
-                        onClick={() => setShowPassword(!showPassword)}
-                        onMouseDown={(e) => e.preventDefault()}
-                        edge="end"
-                      >
-                        {showPassword ? <VisibilityOff /> : <Visibility />}
-                      </IconButton>
-                    </InputAdornment>
-                  ),
-                }}
-              />
-            </Stack>
+					<FormProvider methods={methods} onSubmit={handleSubmit(onSubmit)}>
+						<Stack spacing={3}>
+							<FTextField name="username" label="UserName" />
+							<FTextField
+								name="password"
+								label="password"
+								type={showPassword ? "text" : "password"}
+								InputProps={{
+									endAdornment: (
+										<InputAdornment position="end">
+											<IconButton
+												aria-label="toggle password visibility"
+												onClick={() => setShowPassword(!showPassword)}
+												onMouseDown={(e) => e.preventDefault()}
+												edge="end"
+											>
+												{showPassword ? <VisibilityOff /> : <Visibility />}
+											</IconButton>
+										</InputAdornment>
+									),
+								}}
+							/>
+						</Stack>
 
-            <LoadingButton
-              fullWidth
-              size="large"
-              type="submit"
-              variant="contained"
-              loading={isSubmitting}
-              sx={{ mt: 3 }}
-            >
-              Login
-            </LoadingButton>
-          </FormProvider>
-          <Typography
-            sx={{
-              fontSize: 14,
-              mt: 3,
-              display: "flex",
-              justifyContent: "space-between",
-            }}
-          >
-            <Link sx={{ mr: 4 }} underline="none">
-              Forgot password?
-            </Link>
-            <Link underline="none">Don't have an account? Sign Up</Link>
-          </Typography>
-        </Container>
-      </Modal>
-    </Container>
-  );
-};
+						<LoadingButton
+							fullWidth
+							size="large"
+							type="submit"
+							variant="contained"
+							loading={isSubmitting}
+							sx={{ mt: 3 }}
+						>
+							Login
+						</LoadingButton>
+					</FormProvider>
+					<Typography
+						sx={{
+							fontSize: 14,
+							mt: 3,
+							display: "flex",
+							justifyContent: "space-between",
+						}}
+					>
+						<Link sx={{ mr: 4 }} underline="none">
+							Forgot password?
+						</Link>
+						<Link underline="none">Don't have an account? Sign Up</Link>
+					</Typography>
+				</Container>
+			</Modal>
+		</Container>
+	)
+}
 
-export default SignInPage;
+export default SignInPage
